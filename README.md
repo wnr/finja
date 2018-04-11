fcec45d85761c7d01f9a033583959a9947f3f953
Lucas Wiener <lucas@wiener.se>
2018-04-11 16:15:11 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# finja
+
+Namespace for handling browser URL, History and Address bar.
+
+To get an interactive development environment run:
+
+    lein figwheel
---
+++ src/finja/core.cljs
@@ -0,0 +1,36 @@
+; Namespace for handling browser URL, History and Address bar.
+
+(ns finja.core
+  (:require [goog.events :as events]
+            [goog.history.EventType :as EventType])
+  (:import goog.history.Html5History))
+
+(def state-atom (atom {:on-path-change-listeners #{}}))
+
+(defonce history-instance (let [history-instance (Html5History.)]
+                            (goog.events/listen history-instance EventType/NAVIGATE (fn [event]
+                                                                                      (doseq [listener (:on-path-change-listeners (deref state-atom))]
+                                                                                        (listener event))))
+                            (doto history-instance (.setEnabled true))))
+
+(defn get-current-path
+  []
+  (.getToken history-instance))
+
+(defn set-current-path!
+  [{path :path}]
+  (when (not= (get-current-path)
+              path)
+    (.setToken history-instance path)))
+
+(defn on-path-change
+  ([callback]
+   (on-path-change nil callback))
+  ([options callback]
+   (let [{user-changes-only :user-changes-only} (merge options
+                                                       {:user-changes-only true})]
+     (swap! state-atom update :on-path-change-listeners (fn [listeners]
+                                                          (conj listeners (fn [event]
+                                                                            (when (or (not user-changes-only)
+                                                                                      (.-isNavigation event)) ;; isNavigation = true means that the user made the change.
+                                                                              (callback {:path (.-token event)})))))))))
